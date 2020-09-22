788b39a41af8ac20d8a7bb414dba4201c9c0aeb4
Bladefurey <bladefurey@gmail.com>
2020-09-22 23:11:00 +0630

    assignment

---
+++ js/agency.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    document.getElementById("card1").onmouseover=()=>{
+        document.getElementById("card1").style.backgroundColor = 'blueviolet';  
+        document.getElementById("card-1").style.color="white";
+        document.getElementById("btn1").style.backgroundColor="white";
+    }
+    document.getElementById("card1").onmouseout=()=>{
+    
+        document.getElementById("card1").style.backgroundColor="white";
+        document.getElementById("card-1").style.color="black";
+        document.getElementById("btn1").style.backgroundColor="blueviolet";
+    }
+    document.getElementById("card2").onmouseover=()=>{
+    
+        document.getElementById("card2").style.backgroundColor = 'blueviolet';  
+        document.getElementById("card-2").style.color="white";
+        document.getElementById("btn2").style.backgroundColor="white";
+    
+    }
+    document.getElementById("card2").onmouseout=()=>{
+    
+        document.getElementById("card2").style.backgroundColor="white";
+        document.getElementById("card-2").style.color="black";
+        document.getElementById("btn2").style.backgroundColor="blueviolet";
+    }
+    
+    document.getElementById("card3").onmouseover=()=>{
+
+        document.getElementById("card3").style.backgroundColor = 'blueviolet';  
+        document.getElementById("card-3").style.color="white";
+        document.getElementById("btn3").style.backgroundColor="white"; 
+    }
+    document.getElementById("card3").onmouseout=()=>{
+    
+        document.getElementById("card3").style.backgroundColor="white";
+        document.getElementById("card-3").style.color="black";
+        document.getElementById("btn3").style.backgroundColor="blueviolet";
+    }
+    document.getElementById("card4").onmouseover=()=>{
+    
+        document.getElementById("card4").style.backgroundColor = 'blueviolet';  
+        document.getElementById("card-4").style.color="white";
+        document.getElementById("btn4").style.backgroundColor="white"; 
+    
+    }
+    document.getElementById("card4").onmouseout=()=>{
+    
+        document.getElementById("card4").style.backgroundColor="white";
+        document.getElementById("card-4").style.color="black";
+        document.getElementById("btn4").style.backgroundColor="blueviolet";
+
+    }
+    
+    document.getElementById("card5").onmouseover=()=>{
+    
+        document.getElementById("card5").style.backgroundColor = 'blueviolet';  
+        document.getElementById("card-5").style.color="white";
+        document.getElementById("btn5").style.backgroundColor="white"; 
+    
+    }
+    document.getElementById("card5").onmouseout=()=>{
+    
+        document.getElementById("card5").style.backgroundColor="white";
+        document.getElementById("card-5").style.color="black";
+        document.getElementById("btn5").style.backgroundColor="blueviolet";
+    }
+    document.getElementById("card6").onmouseover=()=>{
+    
+        document.getElementById("card6").style.backgroundColor = 'blueviolet';  
+        document.getElementById("card-6").style.color="white";
+        document.getElementById("btn6").style.backgroundColor="white"; 
+    
+    }
+    document.getElementById("card6").onmouseout=()=>{
+    
+        document.getElementById("card6").style.backgroundColor="white";
+        document.getElementById("card-6").style.color="black";
+        document.getElementById("btn6").style.backgroundColor="blueviolet";
+    }
+    
+    });
